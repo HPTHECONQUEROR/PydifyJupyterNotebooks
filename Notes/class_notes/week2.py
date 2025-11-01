83bc5504b57d49d2ab3c9ef11ef8aa2e5aa1f8fa
HPTHECONQUEROR <hpofficial420@gmail.com>
2025-11-01 20:59:50 +0530

    Class notes has been added

---
+++ Notes/class_notes/week2.py
@@ -0,0 +1,40 @@
+#sum of n natural numbers
+'''
+1+2+3+4
+'''
+
+# given n = 4 
+# output = 10
+
+# 1 -->  1
+# 2 -->  3
+# 3 -->  6
+# 4 -->  10
+
+#range(start,end)
+#range(10)
+
+
+n = int(input())
+load = 0
+for i in range(1,n+1): # 1 2 3 4
+    load = load + i
+
+# load + i = load
+# 0    + 1 = 1
+# 1     + 2 = 3
+# 3     + 3 = 6
+# 6     + 4 = 10
+
+print(load) #10
+
+
+
+
+
+
+
+
+
+
+
